3388bd0c834fa2be36e2c544c7367daf4debe97d
LUPUS001 <antoniodaw23@gmail.com>
2026-06-19 21:56:49 +0200

    4.4.5 Subir imágenes al servidor

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -9,13 +9,26 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('file')
+            ->add('file', FileType::class,[
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ])
             ->add('numLikes', null, ['attr' => ['class' => 'form-control']])
             ->add('numViews', null, ['attr' => ['class' => 'form-control']])
             ->add('numDownloads', null, ['attr' => ['class' => 'form-control']])
