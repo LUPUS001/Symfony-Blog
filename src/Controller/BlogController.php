40398f5ea84062a962c357e09c9b615dd59153df
LUPUS001 <antoniodaw23@gmail.com>
2026-06-22 13:54:28 +0200

    5.3 RETO

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 final class BlogController extends AbstractController
 {
     #[Route('/blog/single_post', name: 'single_post')]
@@ -22,12 +24,36 @@ final class BlogController extends AbstractController
     #[Route('/blog/new', name: 'new_post')]
     public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
     {
+        // Comprobar si el usuario ha iniciado sesión para reenviarlo a login
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
+
+            // Procesar la subida de la imagen
+            $file = $form->get('image')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('blog_images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    return new Response("Error al subir el archivo: " . $e->getMessage());
+                }
+
+                $post->setImage($newFilename);
+            }
 
             // Quitamos los caracteres especiales del título para crear el slug
             $post->setSlug($slugger->slug($post->getTitle()));
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,19 +6,46 @@ use App\Entity\Post;
 // use App\Entity\User;
 // use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
 {
+    // Hacer que los campos title, content e image sean obligatorios
+    // Añadir las clases css a los campos del formulario
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('image')
-            ->add('Send', SubmitType::class)
+            ->add('title', null, [
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            // Crear un campo de tipo file para la imagen, "mapped" es para que no se guarde en la base de datos, si no que se guarde la imagen en la carpeta "public/images/blog"
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('Send', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-default pull-right']
+            ])
         ;
     }
 
