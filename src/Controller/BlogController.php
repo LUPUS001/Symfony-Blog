7d56798c869c9f9fe1a9b8fda74f1542f51c1338
LUPUS001 <antoniodaw23@gmail.com>
2026-06-22 11:23:25 +0200

    5.2 Formulario

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 final class BlogController extends AbstractController
 {
@@ -12,6 +17,37 @@ final class BlogController extends AbstractController
     public function single_post(): Response
     {
         return $this->render('blog/single_post.html.twig');
+    }
+
+    #[Route('/blog/new', name: 'new_post')]
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+
+            // Quitamos los caracteres especiales del título para crear el slug
+            $post->setSlug($slugger->slug($post->getTitle()));
+
+            //Guardamos el usuario que crea el post y los contadores a 0
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->render('blog/new_post.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+        return $this->render('blog/new_post.html.twig', array(
+                'form' => $form->createView()
+        ));
     }
 
     #[Route('/blog', name: 'blog')]
